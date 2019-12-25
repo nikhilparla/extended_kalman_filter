4ad0d2564a9cf8f220feb4b4a787c2194de9e3ee
nikhil <nikhil.parlapalli@magna.com>
2019-12-24 19:17:59 -0500

    moved jacobian calculation from filter to FusionEKF. Updating H matrix too with dt

--- src/FusionEKF.cpp
+++ src/FusionEKF.cpp
@@ -157,12 +157,18 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
 
   if (measurement_pack.sensor_type_ == MeasurementPackage::RADAR) {
     // TODO: Radar updates
+	MatrixXd Hj_ = tools.CalculateJacobian(ekf_.x_);
+	ekf_.H_ = Hj_;
 	ekf_.R_ = R_radar_;
 	ekf_.UpdateEKF(measurement_pack.raw_measurements_);
 
   } else {
     // TODO: Laser updates
 		ekf_.R_ = R_laser_;
+		// Update H_ for laser
+		H_laser_ << 1.0, 0.0, dt, 0.0,
+					0.0, 1.0, 0.0, dt;
+		ekf_.H_ = H_laser_;
 		ekf_.Update(measurement_pack.raw_measurements_);
 	}
 	
--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -41,11 +41,6 @@ void KalmanFilter::Update(const VectorXd &z) {
    VectorXd z_pred = H_ * x_;
    VectorXd y = z - z_pred;
    MatrixXd Ht = H_.transpose();
-/*
-   cout << "H --  lidar  size = "<< H_.size() << endl;
-   cout << "P_ -- lidar size = "<< P_.size() << endl;
-   cout << "R  -- lidar size = "<< R_.size() << endl;
-*/
    MatrixXd S = H_ * P_ * Ht + R_;
    MatrixXd Si = S.inverse();
    MatrixXd PHt = P_ * Ht;
@@ -61,8 +56,6 @@ void KalmanFilter::UpdateEKF(const VectorXd &z) {
   /**
    * TODO: update the state by using Extended Kalman Filter equations
    */
-   Tools tools;
-   MatrixXd Hj = tools.CalculateJacobian(x_);
    
    float c1 = sqrt(x_(0)*x_(0) + x_(1)*x_(1));
    float c2 = atan2(x_(1),x_(0));
@@ -75,21 +68,16 @@ void KalmanFilter::UpdateEKF(const VectorXd &z) {
 			c3/c1;
    
    VectorXd y = z - hx;
-   if((y(1) < -3.14) || (y(1) > 3.14) )
-		cout << "Phi value in rad = " << y(1) << endl;
-   MatrixXd Hjt = Hj.transpose();
-/*   cout << "Hj size = "<< Hj.size() << endl;
-   cout << "P_ size = "<< P_.size() << endl;
-   cout << "R size = "<< R_.size() << endl;
-*/   MatrixXd S = Hj * P_ * Hjt + R_;
+   MatrixXd Ht = H_.transpose();
+   MatrixXd S = H_ * P_ * Ht + R_;
    MatrixXd Si = S.inverse();
-   MatrixXd PHjt = P_ * Hjt;
-   MatrixXd K = PHjt * Si;
+   MatrixXd PHt = P_ * Ht;
+   MatrixXd K = PHt * Si;
    
    // new estimates
    x_ = x_ + (K*y);
    MatrixXd I = MatrixXd::Identity(x_.size(), x_.size());
-   P_ = (I - K * Hj) * P_;
+   P_ = (I - K * H_) * P_;
 }
 
 
